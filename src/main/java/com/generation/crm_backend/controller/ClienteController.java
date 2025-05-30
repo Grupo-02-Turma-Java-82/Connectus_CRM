6e58cb07f5aade7a6207da19b6a74627554bb6b0
Luiza_Gonck <luizae.goncalves@hotmail.com>
2025-05-30 14:39:01 -0300

    :sparkles: Controller Cliente criada

---
+++ src/main/java/com/generation/crm_backend/controller/ClienteController.java
@@ -0,0 +1,60 @@
+package com.generation.crm_backend.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.generation.crm_backend.repository.ClienteRepository;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+
+@RestController
+@RequestMapping("/clientes")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class ClienteController {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    @GetMapping
+    public List<Cliente> getAll() {
+        return clienteRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Cliente> getById(@PathVariable Long id) {
+        return clienteRepository.findById(id)
+        .map(resp -> ResponseEntity.ok(resp))
+        .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<Cliente> post(@RequestBody Cliente cliente) {
+       return ResponseEntity.status(HttpStatus.CREATED)
+                .body(clienteRepository.save(cliente));
+    }
+
+    @PutMapping
+    public ResponseEntity<Cliente> put(@RequestBody Cliente cliente) {
+        return ResponseEntity.ok(clienteRepository.save(cliente));
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        clienteRepository.deleteById(id);
+    }
+    
+}
