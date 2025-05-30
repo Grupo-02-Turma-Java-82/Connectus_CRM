ce6cb2a6ecfbe9f742fb655d8387d4f885bb0d0e
Felipe Peronica <felipeperonica@gmail.com>
2025-05-30 15:39:37 -0300

    :tada: adicionado o Usuario Repository

---
+++ src/main/java/com/generation/crm_backend/Repository/UsuarioRepository.java
@@ -0,0 +1,14 @@
+package com.generation.crm_backend.Repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.generation.crm_backend.model.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+
+	public Optional<Usuario> findByEmail(String email);
+	
+	
+}
