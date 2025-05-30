fbec7b1382a32b05f3326d8f6215472a8f066c84
JovaniOUnico <jovani.souza.almeida@gmail.com>
2025-05-30 14:34:04 -0300

    :tada: adicionando model Usuario

---
+++ src/main/java/com/generation/crm_backend/model/Usuario.java
@@ -0,0 +1,111 @@
+package com.generation.crm_backend.model;
+
+//import java.util.List;
+
+//import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+//import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+//import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+//import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_usuarios")
+public class Usuario {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotBlank(message = "O Atributo Nome é Obrigatório!")
+	private String nome;
+
+	@NotNull(message = "O atributo Email é Obrigatório!")
+	@Email(message = "O atributo Email deve ser válido!")
+	private String email;
+
+	@NotBlank(message = "O Atributo Senha é Obrigatório!")
+	@Size(min = 8, message = "A Senha deve ter no mínimo 8 caracteres")
+	private String senha;
+
+	@Size(max = 5000, message = "O link da foto não pode ser maior do que 5000 caracteres")
+	private String foto;
+
+	@Size(min = 10, message = "o Telefone deve ter no mínimo 10 caracteres")
+	private String telefone;
+	
+	@NotNull(message = "O atributo cargo é Obrigatório!")
+	private String cargo;
+
+	//@OneToMany(fetch = FetchType.LAZY, mappedBy = "usuario", cascade = CascadeType.REMOVE)
+	//@JsonIgnoreProperties("usuario")
+	//private List<Postagem> postagem;
+
+	/* Insira os Getters and Setters */
+
+	public Long getId() {
+		return this.id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return this.nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return this.email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	//TODO colocar criptografia na senha
+	public String getSenha() {
+		return this.senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getFoto() {
+		return this.foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+	
+	public String getTelefone() {
+		return this.telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getCargo() {
+		return this.cargo;
+	}
+
+	public void setCargo(String cargo) {
+		this.foto = cargo;
+	}
+
+}
